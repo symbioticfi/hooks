d4fcf0c541c5fbabe92e88d02072547954a8c9bf
Kresh <andrey@symbiotic.fi>
2024-09-20 02:54:30 +0800

    feat: add fair hook for NetworkRestakeDelegator

---
+++ src/contracts/NetworkRestakeFairHook.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {INetworkRestakeFairHook} from "../interfaces/INetworkRestakeFairHook.sol";
+
+import {IBaseSlasher} from "@symbioticfi/core/src/interfaces/slasher/IBaseSlasher.sol";
+import {IEntity} from "@symbioticfi/core/src/interfaces/common/IEntity.sol";
+import {IVault} from "@symbioticfi/core/src/interfaces/vault/IVault.sol";
+import {IDelegatorHook} from "@symbioticfi/core/src/interfaces/delegator/IDelegatorHook.sol";
+import {INetworkRestakeDelegator} from "@symbioticfi/core/src/interfaces/delegator/INetworkRestakeDelegator.sol";
+
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract NetworkRestakeFairHook is INetworkRestakeFairHook {
+    using Math for uint256;
+
+    /**
+     * @inheritdoc IDelegatorHook
+     */
+    function onSlash(
+        bytes32 subnetwork,
+        address operator,
+        uint256 slashedAmount,
+        uint48 captureTimestamp,
+        bytes calldata data
+    ) external {
+        if (IEntity(msg.sender).TYPE() != 0) {
+            revert NotNetworkRestakeDelegator();
+        }
+
+        if (slashedAmount == 0) {
+            return;
+        }
+
+        address vault = INetworkRestakeDelegator(msg.sender).vault();
+        address slasher = IVault(vault).slasher();
+
+        uint256 prevSlashableStake = INetworkRestakeDelegator(msg.sender).stakeAt(
+            subnetwork, operator, captureTimestamp, new bytes(0)
+        )
+            - (
+                (IBaseSlasher(slasher).cumulativeSlash(subnetwork, operator) - slashedAmount)
+                    - IBaseSlasher(slasher).cumulativeSlashAt(subnetwork, operator, captureTimestamp, new bytes(0))
+            );
+
+        INetworkRestakeDelegator(msg.sender).setOperatorNetworkShares(
+            subnetwork,
+            operator,
+            (prevSlashableStake - slashedAmount).mulDiv(
+                INetworkRestakeDelegator(msg.sender).operatorNetworkShares(subnetwork, operator), prevSlashableStake
+            )
+        );
+    }
+}
